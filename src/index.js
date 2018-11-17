07e455ca044fc8936dfb7a97dabb7f26bee43fda
Charlie <dev@dev.com>
2018-11-16 21:17:07 -0800

    Adding Video site components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
-const API_KEY =  'AIzaSyBkizG7OX4sSPqgRr1NUSMwJrVIHCqTi5c';
+const API_KEY =  '';
 
 //Create a new component. This should produce some HTML
 const App = () => {
