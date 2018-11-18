384aadcc5616015fcd98d31074ed2f748a98fe87
Charlie <dev@dev.com>
2018-11-17 16:40:43 -0800

    Connecting to youtube via API key and retrieving data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY =  '';
+const API_KEY = '';
 
+
+YTSearch({key: API_KEY, term: 'surfboard'}, function(data){
+    console.log(data);
+})
 //Create a new component. This should produce some HTML
 const App = () => {
     return (
