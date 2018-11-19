95ff00aea02e28cea70f8ef61d5cb6d6ff2802f4
Charlie <dev@dev.com>
2018-11-18 20:45:57 -0800

    Video items and video item lists

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAdcrkgkzCPMRZIWXnrHtjmVPMM0pPNOMQ';
-
-
 
 //Create a new component. This should produce some HTML
 class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos:[]};
+        this.state = { videos: []};
 
         YTSearch({key: API_KEY, term: 'surfboard'}, (data) => {
-            this.setState({videos: data});
-        })
+            this.setState({videos : data});
+        });
     }
     render()
     {
         return (
             <div>
-                <SearchBar/>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
