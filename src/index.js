275714f1038cf2b3f022401de7fc7674bb486904
Charlie <dev@dev.com>
2018-11-19 22:02:51 -0800

    Adding term searching from input Auto search term as state updates Added styling Fixed bootstrap with new CDN

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAdcrkgkzCPMRZIWXnrHtjmVPMM0pPNOMQ';
 
@@ -11,18 +13,32 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards')
 
-        YTSearch({key: API_KEY, term: 'surfboard'}, (data) => {
-            this.setState({videos : data});
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
+
     render()
     {
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                           videos={this.state.videos} />
+
             </div>
         );
     }
